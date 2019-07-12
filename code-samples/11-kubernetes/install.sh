5f0a8f17b0afee58635c3a1791a69e45b0c2ffbd
Gaurav Agarwal <algogrit@gmail.com>
2019-07-12 11:49:03 +0530

    Reordering installation.

--- code-samples/11-kubernetes/install.sh
+++ code-samples/11-kubernetes/install.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-echo "Installing kubectl..." # https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-linux
-
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-sudo ln -sf /usr/local/bin/kubectl /usr/bin/kubectl
-
 echo "Installing docker-machine..." # https://computingforgeeks.com/installing-docker-ce-ubuntu-debian-fedora-arch-centos/
 
 sudo yum remove docker docker-common docker-selinux docker-engine
@@ -16,6 +9,13 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum -y install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker && sudo systemctl enable docker
+
+echo "Installing kubectl..." # https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-linux
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+sudo ln -sf /usr/local/bin/kubectl /usr/bin/kubectl
 
 echo "Installing kvm..." # https://computingforgeeks.com/how-to-run-minikube-on-kvm/
 
