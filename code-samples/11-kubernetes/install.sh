cd29a4d0d43520ac2f249b6e70cfe9c2b196fca0
Gaurav Agarwal <algogrit@gmail.com>
2019-07-12 10:51:58 +0530

    Updating install script.

--- code-samples/11-kubernetes/install.sh
+++ code-samples/11-kubernetes/install.sh
@@ -12,9 +12,26 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 sudo ln -s /usr/local/bin/kubectl /usr/bin/kubectl
 
-echo "Installing kvm..."
+echo "Installing docker-machine..." # https://computingforgeeks.com/installing-docker-ce-ubuntu-debian-fedora-arch-centos/
 
-sudo yum install kvm
+sudo yum remove docker docker-common docker-selinux docker-engine
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker && sudo systemctl enable docker
+
+
+echo "Installing kvm..." # https://computingforgeeks.com/how-to-run-minikube-on-kvm/
+
+sudo yum -y install epel-release
+sudo yum -y install libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils
+
+sudo systemctl start libvirtd
+sudo systemctl enable libvirtd
 
 echo "Installing minikube..." # https://kubernetes.io/docs/tasks/tools/install-minikube/#install-minikube-via-direct-download
 
@@ -23,6 +40,14 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 sudo install minikube /usr/local/bin
 
 rm minikube
+
+echo "Installing docker-machine-driver-kvm2..."
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2
+
+chmod +x docker-machine-driver-kvm2
+
+sudo mv docker-machine-driver-kvm2 /usr/local/bin/
 
 echo "Installation done! Please verfiy by running..."
 echo "kubectl version"
