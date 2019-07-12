1bfbf06d7d32e76aace532a6dc0a941922b13e06
Gaurav Agarwal <algogrit@gmail.com>
2019-07-12 10:17:16 +0530

    Adding a simple installation script.

---
+++ code-samples/11-kubernetes/installation.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Installing docker-compose..."
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+echo "Installing kubectl..."
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+sudo ln -s /usr/local/bin/kubectl /usr/bin/kubectl
+
+echo "Installing kvm..."
+
+sudo yum install kvm
+
+echo "Installing minikube..."
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+
+sudo install minikube /usr/local/bin
