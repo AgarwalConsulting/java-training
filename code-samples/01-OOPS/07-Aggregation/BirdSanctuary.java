d18a1c06a7b43f588e5069afcae3decb05789f03
Gaurav Agarwal <algogrit@gmail.com>
2019-07-15 14:08:03 +0530

    Updating example.

--- code-samples/01-OOPS/07-Aggregation/BirdSanctuary.java
+++ code-samples/01-OOPS/07-Aggregation/BirdSanctuary.java
@@ -1,7 +1,17 @@
+import java.util.*;
+
 public class BirdSanctuary {
-  Eagle eagle = new Eagle();
+  List<Eagle> birds = new ArrayList<Eagle>();
+
+  BirdSanctuary() {
+    System.out.println("Adding an eagle...");
+    birds.add(new Eagle());
+  }
 
   public void list() {
-    eagle.flyUp();
+    System.out.println("Flying all birds...");
+    for (Eagle bird : birds) {
+      bird.flyUp();
+    }
   }
 }
