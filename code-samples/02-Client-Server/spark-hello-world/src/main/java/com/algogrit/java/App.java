44741fa9082f984467aafe15b37bd6201b6e9a2a
Gaurav Agarwal <algogrit@gmail.com>
2019-07-16 12:07:36 +0530

    Adding student example.

---
+++ code-samples/02-Client-Server/spark-hello-world/src/main/java/com/algogrit/java/App.java
@@ -0,0 +1,46 @@
+package com.algogrit.java;
+
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        AtomicInteger counter = new AtomicInteger();
+        HashMap<Integer, Student> students = new HashMap<Integer, Student>();
+
+        get("/students", (req, res) -> gson.toJson(students.values()));
+
+        get("/students/:id", (req, res) -> {
+            Integer studentId = Integer.parseInt(req.params(":id"));
+            Student student = students.get(studentId);
+
+            if (student == null ) {
+                res.status(404);
+                return null;
+            }
+
+            return gson.toJson(student);
+        });
+
+        post("/students", (req, res) -> {
+            Student newStudent = gson.fromJson(req.body(), Student.class);
+
+            Integer newId = counter.getAndIncrement();
+            newStudent.setId(newId);
+            students.put(newId, newStudent);
+
+            return gson.toJson(newStudent);
+        });
+
+        delete("/students/:id", (req, res) -> {
+            Integer studentId = Integer.parseInt(req.params(":id"));
+
+            return gson.toJson(students.remove(studentId));
+        });
+    }
+}
---
+++ code-samples/02-Client-Server/spark-hello-world/src/main/java/com/algogrit/java/Student.java
@@ -0,0 +1,40 @@
+package com.algogrit.java;
+
+public class Student {
+  private Integer id;
+  private String firstName;
+  private String lastName;
+  private String phoneNumber;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+}
