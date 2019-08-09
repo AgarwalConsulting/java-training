50e0ad6e4875ed662ed49272c149c0f14b04f4ae
Gaurav Agarwal <algogrit@gmail.com>
2019-08-09 15:33:34 +0530

    Adding check.

--- code-samples/11-kubernetes/build-image.sh
+++ code-samples/11-kubernetes/build-image.sh
@@ -7,9 +7,11 @@ mkdir -p ~/.docker
 cp docker-config.json ~/.docker/config.json
 
 echo "Fetching kubernetes source..."
-mkdir -p ~/.go/src/k8s.io/kubernetes
+mkdir -p ~/.go/src/k8s.io
 export GOPATH=$HOME/.go
-git clone https://github.com/kubernetes/kubernetes.git ~/.go/src/k8s.io/kubernetes
+if [ ! -d ~/.go/src/k8s.io/kubernetes ]; then
+  git clone https://github.com/kubernetes/kubernetes.git ~/.go/src/k8s.io/kubernetes
+fi
 
 echo "Creating kind cluster..."
 # kind delete cluster
